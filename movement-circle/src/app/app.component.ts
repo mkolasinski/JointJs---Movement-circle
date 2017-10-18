059c52d8c79500e0a365fcb962ab7d1a31c8fddf
mkolasinski <m.kolasinski@poczta.fm>
2017-10-18 12:56:48 +0200

    - earth on the ellipse... it have to be!

--- movement-circle/src/app/app.component.ts
+++ movement-circle/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import * as $ from "jquery";
 import {dia, g, shapes, V} from "jointjs";
+import setPrototypeOf = Reflect.setPrototypeOf;
 
 
 @Component({
@@ -12,15 +13,15 @@ import {dia, g, shapes, V} from "jointjs";
 export class AppComponent implements OnInit{
 
   ngOnInit() {
-    let constraint = g.ellipse(g.point(200, 190), 100, 80);
 
+    let constraint = g.ellipse(g.point(200,180), 120, 80);
     let ConstraintElementView = dia.ElementView.extend({
 
       pointerdown: function (evt, x, y) {
         let position = this.model.get('position');
         let size = this.model.get('size');
         let center = g.rect(position.x, position.y, size.width, size.height).center();
-        let intersection = constraint.intersectionWithLineFromCenterToPoint(center, 0);
+        let intersection = constraint.intersectionWithLineFromCenterToPoint(g.point(x,y), 0);
         dia.ElementView.prototype.pointerdown.apply(this, [evt, intersection.x, intersection.y]);
       },
 
@@ -40,22 +41,24 @@ export class AppComponent implements OnInit{
       elementView: ConstraintElementView
     });
 
-    let orbit = V('<ellipse></ellipse>');
-    orbit.attr({
-      cx: constraint.x, cy: constraint.y, rx: constraint.a, ry: constraint.b,
-      fill: '#ECF0F1', stroke: '#34495E', 'stroke-dasharray': [2, 2]
-    });
-    V(paper.viewport).append(orbit);
+      let orbit = V('<ellipse/>');
+      orbit.attr({
+          cx: 200, cy: 175, rx: constraint.a, ry: constraint.b,
+          fill: '#ECF0F1', stroke: '#34495E', 'stroke-dasharray': [1, 2]
+      });
+      V(paper.viewport).append(orbit);
 
-    let earth = new shapes.basic.Circle({
-      position: constraint.intersectionWithLineFromCenterToPoint(g.point(0, 0), 0).offset(-40, -10),
-      size: {width: 50, height: 50},
-      attrs: {
-        text: {text: 'Earth', 'font-family': 'Papyrus', 'font-size': 13, 'font-weight': 'bold', fill: 'darkblue'},
-        circle: {fill: '#2ECC71', stroke: '#095E06', 'stroke-width': 1}
-      },
-      name: 'earth'
-    });
+      let earth: shapes.basic.Circle;
+      earth = new shapes.basic.Circle({
+          // position: { x: constraint.x, y: constraint.y},
+          position: constraint.intersectionWithLineFromCenterToPoint(g.point(0, 0), 0).offset(-70, 0),
+          size: {width: 50, height: 50},
+          attrs: {
+              text: {text: 'Earth', 'font-family': 'Papyrus', 'font-size': 13, 'font-weight': 'bold', fill: 'darkblue'},
+              circle: {fill: '#2ECC71', stroke: '#095E06', 'stroke-width': 1}
+          },
+          name: 'earth'
+      });
     graph.addCell(earth);
   }
 }
